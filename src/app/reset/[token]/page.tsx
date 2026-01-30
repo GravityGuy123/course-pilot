a26f45614afc206e566771d449cca865cd19ec30
GravityGuy123 <simmyifeanyi@gmail.com>
2026-01-30 14:03:33 +0100

    fix: refactor Verify Email and Reset Password Page

---
+++ src/app/reset/[token]/page.tsx
@@ -0,0 +1,8 @@
+export default function ResetPasswordPage() {
+
+    return (
+        <section>
+            <h1>Reset Password</h1>
+        </section>
+    )
+}
---
+++ src/app/verify-email/page.tsx
@@ -0,0 +1,9 @@
+export default function VerifyEmailPage() {
+
+    return (
+        <section>
+            <h1>Verify Email</h1>
+            <p>Please check your email to verify your account.</p>
+        </section>
+    )
+}
